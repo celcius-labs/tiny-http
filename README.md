bebbfd199c22b08310c942afc21d5a7034e0f186
Jerry Sievert <code@legitimatesounding.com>
2016-01-28 15:23:58 -0800

    get ready for publication on platformio

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Tiny-HTTP
 
-A super tiny HTTP server targeting embedded devices.
+A super tiny HTTP server toolkit targeting embedded devices.
 
 ## Goals
 
@@ -17,3 +17,40 @@ A super tiny HTTP server targeting embedded devices.
 * Parse parameters
 * Parse headers
 * Lots of tests
+
+## Using
+
+The HTTP Parser is stable, and can be used as-is.  It will parse the request
+into a `method`, `path`, `parameters`, and `headers`.
+
+`Tiny-HTTP` is opinionated, and uses unsigned 8 bit integers in the form of
+`uint8_t`.  It is also meant to work in as many environments as possible, and
+thus has a more `C` style to it.
+### Request
+
+A `Request` contains a `method`, `path`, `parameters`, and `headers`.  If an
+error occurs, or an unsupported method is sent, `method` is set to `HTTP_ERROR`.
+
+* `method` - `uint8_t` method (`HTTP_GET`, `HTTP_POST`, `HTTP_PUT`, `HTTP_ERROR`)
+* `path` - `uint8_t *` path or NULL on error
+* `params` - `uint8_t **` array of parameters NULL terminated (`foo=bar`)
+* `headers` - `uint8_t **` array of headers NULL terminated (`Content-type: application/json`)
+
+
+### parse_request(uint8_t \*req)
+
+Parses a request, returning a `Request` object.
+
+_param_ req - `uint8_t *` buffer containing the full request
+_returns_ Request - `Request *` request that contains a `Request` object.
+
+_example_:
+
+```
+uint8_t buf[128] = "GET /foo?bar=baz HTTP/1.1\r\n\r\n";
+
+Request *request = http_parser(buf);
+
+Serial.println((char *) request->path); // /foo
+Serial.println((char *) request->params[0]); // bar=baz
+```
---
+++ examples/arduino-esp8266/main.ino
@@ -0,0 +1,103 @@
+#include <ESP8266WiFi.h>
+
+#include "http_parser.h"
+
+#define SSID "Hometown"
+#define PASSWORD "z1ppy!234"
+
+// timeout of 5 seconds
+#define TIMEOUT 5000
+
+// maximum request size, defaults to 1k
+#define MAX_REQUEST_SIZE 1024
+
+WiFiClient client;
+WiFiServer server(80);
+
+void setup ( ) {
+  Serial.begin(115200);
+
+  Serial.print("\n\nConnecting to ");
+  Serial.println(SSID);
+
+  WiFi.begin(SSID, PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+
+    delay(250);
+  }
+
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.begin();
+
+  delay(25);
+}
+
+// read the request and return the results for parsing
+uint8_t *readRequest (WiFiClient client) {
+  static uint8_t buf[MAX_REQUEST_SIZE];
+  int crlfcount = 0, position = 0;
+  char c;
+
+  // while there is still data, read it
+  while (client.connected() && client.available() && position < MAX_REQUEST_SIZE && crlfcount < 4) {
+    c = client.read();
+
+    if (c == '\n' || c == '\r') {
+      crlfcount++;
+    } else {
+      crlfcount = 0;
+    }
+
+    buf[position] = (uint8_t) c;
+    position++;
+  }
+
+  if (position == 0) {
+    return NULL;
+  } else {
+    buf[position] = '\0';
+    return buf;
+  }
+}
+
+
+void loop ( ) {
+  // Check if a client has connected
+  WiFiClient connection = server.available();
+
+  if (!connection) {
+    delay(25);
+    return;
+  }
+
+  // read the request, returns a buffer
+  uint8_t *buf = readRequest(connection);
+
+  // if there's an error, bail
+  if (buf == NULL) {
+    connection.flush();
+    delay(25);
+    return;
+  }
+
+  // parse the request, get the results back
+  Request *request = parse_request(buf);
+
+  // this allows for better path matching
+  String path = String((char *) request->path);
+
+  if (path.compareTo("/ping")) {
+    connection.println("HTTP/1.1 200 OK\r\nContent-type: text/plain\r\n\r\npong\r\n");
+  } else {
+    connection.println("HTTP/1.1 404 Not Found\r\n");
+  }
+
+  connection.flush();
+
+  delay(25);
+}
